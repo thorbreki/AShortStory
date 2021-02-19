4c7ed4e3272d22e0ec0cc92ee2567356203b48a4
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2021-02-19 00:07:31 +0000

    the projectile now has inaccuracy!

--- A_Short_Story/Assets/Scripts/Player/ProjectileController.cs
+++ A_Short_Story/Assets/Scripts/Player/ProjectileController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 0f;
+    public float upgradeableSpeed = 1f;
 
-    private Vector2 targetDirection; // The target direction the projectile shot by the staff tries to travel in (mouse Position)
+    private Vector2 targetVector; // The target direction the projectile shot by the staff tries to travel in (mouse Position)
+    private Vector2 imperfectionVector; // The vector that will be perpendicular to the target vector in order to create the inaccuracy factor
     private Rigidbody2D rigidBody; // the rigid body component of the projectile
 
     private void Start()
@@ -24,19 +26,19 @@ public class ProjectileController : MonoBehaviour
     // MOVE TOWARDS THE CURSOR BUT WITH IMPERFECT ACCURACY
     private void Move()
     {
-        targetDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-        rigidBody.velocity = targetDirection.normalized * movementSpeed;
-        float imperfection = Random.Range(0, 10);
-        int positiveOrNegative = Random.Range(0, 2);
+        targetVector = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position); // The target vector points towards the mouse position
+        imperfectionVector.x = targetVector.normalized.y; // The imperfectionVector is the targetVector rotated clockwise by 90° by saying x = y
+        imperfectionVector.y = -targetVector.normalized.x; // and y = -x
+
+        float imperfection = Random.Range(1f, 1f); // How much the projectile will stray away from the mouse position
+        int positiveOrNegative = Random.Range(0, 2); // 0 = projectile will stray counter-clockwise, 1 = projectile will stray clockwise
+
         if (positiveOrNegative == 0)
         {
-            // transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -imperfection));
-            rigidBody.SetRotation(Quaternion.Euler(new Vector3(0f, 0f, -imperfection)));
-        }
-        else
+            rigidBody.velocity = (targetVector.normalized * movementSpeed * upgradeableSpeed) + (imperfectionVector.normalized * imperfection * upgradeableSpeed);
+        } else
         {
-            // transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, imperfection));
-            rigidBody.SetRotation(Quaternion.Euler(new Vector3(0f, 0f, imperfection)));
+            rigidBody.velocity = (targetVector.normalized * movementSpeed * upgradeableSpeed) - (imperfectionVector.normalized * imperfection * upgradeableSpeed);
         }
     }
 
