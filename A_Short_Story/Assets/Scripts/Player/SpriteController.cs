9340a36ce11d015b5cb66108d051e57400074ca4
thorbreki1199 <40344038+thorbreki@users.noreply.github.com>
2021-02-21 23:50:42 +0000

    GameManager is here and the super basic walking animation for the player

---
+++ A_Short_Story/Assets/Scripts/Player/SpriteController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    private Animator animator; // The animator that is attached to the player object's sprite
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleWalkAnimation();
+    }
+
+    // HANDLES THE PLAYER'S WALKING ANIMATION, MANIPULATES THE isWalking BOOL IN THE ANIMATOR
+    private void HandleWalkAnimation()
+    {
+        // only change the isWalking bool if it is false, not when it is true already
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
+    }
+}
